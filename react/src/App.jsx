cebd3efb871f6489f818e401cc63398a434f5d7d
RohanPathak2004 <rohanpathak258@gamil.com>
2025-10-05 16:32:42 +0530

    initial commit

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Jobs from "./Jobs/Jobs";
 import Internships from "./Internships/Internships";
+import AuthPage from "./LoginAndSignUp/AuthPage.jsx";
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/jobpostings" element={<Jobs />}></Route>
         <Route path="/internships" element={<Internships />}></Route>
+          <Route path="/Auth" element={<AuthPage/>}></Route>
       </Routes>
-    </Router>
+    </>
   );
 };
 
