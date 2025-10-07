a5efc241ffd1f8c3d215ddf54af3beeee928baf6
Priyanshu61103 <priyanshuparashar108@gmail.com>
2025-10-08 00:29:55 +0530

    eighth commit

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -7,19 +7,20 @@ import AuthPage from "./LoginAndSignUp/AuthPage.jsx";
 import {Provider} from "react-redux";
 import {store} from "./Redux/Store/store.js";
 import Resume from "./Resume/Resume.jsx";
-
+import Jobs2 from "./Jobs Host/Jobs2.jsx";
+import Internships2 from "./Internships Host/Internships2.jsx"
 const App = () => {
     return (
         <Provider store={store}>
         <Router>
-
-
                 <Routes>
                     <Route path="/" element={<Home/>}></Route>
                     <Route path="/jobpostings" element={<Jobs/>}></Route>
                     <Route path="/internships" element={<Internships/>}></Route>
                     <Route path="/Auth" element={<AuthPage/>}></Route>
                     <Route path="/resume" element={<Resume/>}></Route>
+                    <Route path="/jobhost" element={<Jobs2/>}></Route>
+                    <Route path="/internshiphost" element={<Internships2/>}></Route>
                 </Routes>
 
         </Router>
