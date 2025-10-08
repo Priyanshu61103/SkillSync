902ca8038d1fb7d5fe935aa3480dd0a6cf10729e
Priyanshu61103 <priyanshuparashar108@gmail.com>
2025-10-09 00:19:43 +0530

    twelth  commit

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,36 +1,16 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route, BrowserRouter} from "react-router-dom";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Home from "./Home";
 import Jobs from "./Jobs/Jobs";
 import Internships from "./Internships/Internships";
 import AuthPage from "./LoginAndSignUp/AuthPage.jsx";
-import { store } from "./Redux/Store/store"
+import { store } from "./Redux/Store/store";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import Jobs2 from "./Jobs Host/Jobs2";
-import Internships2 from "./Internships Host/Internships2"
+import Internships2 from "./Internships Host/Internships2";
 import Resume from "./Resume/Resume.jsx";
-<<<<<<< HEAD
-import Jobs2 from "./Jobs Host/Jobs2.jsx";
-import Internships2 from "./Internships Host/Internships2.jsx"
-import Interview from "./Interview/Interview.jsx";
-const App = () => {
-    return (
-        <Provider store={store}>
-        <Router>
-                <Routes>
-                    <Route path="/" element={<Home/>}></Route>
-                    <Route path="/jobpostings" element={<Jobs/>}></Route>
-                    <Route path="/internships" element={<Internships/>}></Route>
-                    <Route path="/Auth" element={<AuthPage/>}></Route>
-                    <Route path="/resume" element={<Resume/>}></Route>
-                    <Route path="/jobhost" element={<Jobs2/>}></Route>
-                    <Route path="/internshiphost" element={<Internships2/>}></Route>
-                    <Route path = "/aiinterview" element = {<Interview/>}></Route>
-                </Routes>
-=======
 import "react-toastify/dist/ReactToastify.css";
->>>>>>> refs/remotes/origin/main
 
 const App = () => {
   return (
@@ -40,10 +20,11 @@ const App = () => {
           <Route path="/" element={<Home />}></Route>
           <Route path="/jobpostings" element={<Jobs />}></Route>
           <Route path="/internships" element={<Internships />}></Route>
-          <Route path="/jobhost" element={<Jobs2/>}></Route>
-          <Route path="/internshiphost" element={<Internships2/>}></Route>
-          <Route path="/Auth" element={<AuthPage/>}></Route>
-          <Route path="/resume" element={<Resume/>}></Route>
+          <Route path="/Auth" element={<AuthPage />}></Route>
+          <Route path="/resume" element={<Resume />}></Route>
+          <Route path="/jobhost" element={<Jobs2 />}></Route>
+          <Route path="/internshiphost" element={<Internships2 />}></Route>
+          <Route path="/aiinterview" element={<Interview />}></Route>
         </Routes>
         <ToastContainer
           position="top-right"
@@ -53,7 +34,7 @@ const App = () => {
           closeOnClick
           pauseOnHover
           draggable
-          theme="dark" 
+          theme="dark"
         />
       </Router>
     </Provider>
